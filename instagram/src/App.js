1266d77adafa2afcc53c37710cced61f342fdcae
Imon Ovbude <imonovbude@gmail.com>
2019-01-21 17:00:54 -0800

    created SearchBar.js, PostContainer.js, CommentSection.js and imported SearchBar.js and PostContainer.js into App.js

---
+++ instagram/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import SearchBar from './components/SearchBar';
+import PostContainer from './components/PostContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+        <PostContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
